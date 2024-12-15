ca62c33e31d34277aadfbe1389d0f33aa2b9f5e4
ulinycoin <156083970+ulinycoin@users.noreply.github.com>
2024-12-15 23:01:13 +0200

    Update config.py

--- config.py
+++ config.py
@@ -4,5 +4,5 @@ BOT_TOKEN = "7894628510:AAEGifnRrQuelGbyuk9wzrR8nLHsdZ6Dev0"
 # Путь к базе данных (SQLite)
 DATABASE_URL = "sqlite:///diet_bot.db"
 
-OPENAI_API_KEY = "sk-OxBiDOcpNiO7Z_Xb1rLF1I7UxcnD94s5h-0QRlfJVET3BlbkFJiAIuAnu-gIlv4yX8EBLNxtqa8oWEgzjSGLeKRVBHEA"
+OPENAI_API_KEY = "s"
 
